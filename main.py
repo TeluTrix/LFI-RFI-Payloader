8ed7f3b24d904c8f420e9defd71fd4768ea38bba
Luca Albrecht <lal126594@iet-gibb.ch>
2022-04-24 12:50:49 +0200

    Added github link, url&payload amount output, dummy urls&paylods

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import os
 urls = []
 payloads = []
 
-print("LFI/RFI Payload Loader - google.com")
+print("LFI/RFI Payload Loader - github.com/TeluTrix/LFI-RFI-Payloader")
 
 #Get Paths
-urlsPath = input("Drag the urls file in: ")
-payloadsPath = input("Drag the payloads file in: ")
+urlsPath = input("Drag the urls file in / or enter its path: ")
+payloadsPath = input("Drag the payloads file in / or enter its path: ")
 
 #Get urls in an array
 urlsFile = open(urlsPath, "r")
@@ -19,3 +19,6 @@ urls = urlsFile.readlines()
 #Get payloads in an array
 payloadsFile = open(payloadsPath, "r")
 payloads = payloadsFile.readlines()
+
+print("[" + str(len(urls)) + "] - Urls Loaded")
+print("[" + str(len(payloads)) + "] - Payloads Loaded")
